1446dcd22da5f114941981d870f99d814dfac786
anzxfukuoka <anzx0f@gmail.com>
2023-12-17 15:54:37 +0100

    frag shader updated

--- resources/default_frag.glsl
+++ resources/default_frag.glsl
@@ -4,6 +4,11 @@ out vec4 fragColor;
 in vec4 myVarOut;
 
 const float PI = 3.1415926535897932384626433832795;
+
+/*
+particle densitiy
+normalized param
+*/
 in float densitiy;
 
 void main()  
@@ -12,5 +17,15 @@ void main()
     float transparancy = 1 - sin(sqrt(disToCenter * PI)); // sin(sqrt(gl_PointCoord.x * gl_PointCoord.x + gl_PointCoord.y * gl_PointCoord.y) * PI);// + sin(gl_PointCoord.y * PI);
     transparancy = min(pow(transparancy, 2), 0.28f);
 
-    fragColor = vec4(0.1f, 1.0f, densitiy, transparancy * 2); //* vec4(gl_PointCoord.x, gl_PointCoord.y, 0.0f, 1.0f); //  myVarOut * ;
+    vec4 color = vec4(0.0f, 0.0f, 1.0f, transparancy * 2);
+
+    if(densitiy > 0.5f)
+    {
+        color.r += pow(densitiy, 4);
+    }else
+    {
+        color.g += pow(1 - densitiy, 4);
+    }
+
+    fragColor = color;//* vec4(gl_PointCoord.x, gl_PointCoord.y, 0.0f, 1.0f); //  myVarOut * ;
 };
