e0b9309dee1c834794f1f586a5af3ce0f0274f02
anzxfukuoka <anzx0f@gmail.com>
2023-12-17 11:58:22 +0100

    blurred point shader

--- resources/default_frag.glsl
+++ resources/default_frag.glsl
@@ -1,7 +1,16 @@
 #version 330 core
 out vec4 fragColor;
 
+in vec4 myVarOut;
+
+const float PI = 3.1415926535897932384626433832795;
+
+
 void main()  
 {
-    fragColor = vec4(0.0f, 1.0f, 0.8f, 1.0f);
+    float disToCenter = distance(gl_PointCoord, vec2(0.5f, 0.5f));  
+    float transparancy = 1 - sin(sqrt(disToCenter * PI)); // sin(sqrt(gl_PointCoord.x * gl_PointCoord.x + gl_PointCoord.y * gl_PointCoord.y) * PI);// + sin(gl_PointCoord.y * PI);
+    transparancy = pow(transparancy, 2);
+
+    fragColor = vec4(0.6f, 0.8f, 1.0f, transparancy); //* vec4(gl_PointCoord.x, gl_PointCoord.y, 0.0f, 1.0f); //  myVarOut * ;
 };
--- resources/default_vert.glsl
+++ resources/default_vert.glsl
@@ -1,7 +1,11 @@
 #version 330 core
 layout (location = 0) in vec3 inPosition;
 
+uniform vec4 myVar = {0.5, 0.2, 0.7, 1.0};
+out vec4 myVarOut;
+
 void main()  
 {
     gl_Position = vec4(inPosition, 1.0);
+    myVarOut = myVar;
 };
